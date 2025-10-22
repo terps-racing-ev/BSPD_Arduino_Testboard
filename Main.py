8088975269202ea61cdcdd3ea5f386ca22b942e1
AlexChapin <Achapin@nbinteractive.com>
2025-10-22 16:21:08 -0400

    Update Baud Rate

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import serial
 
-from tkinter import Button, Canvas, Label, Radiobutton, Scale, StringVar, Tk, IntVar
+from tkinter import HORIZONTAL, Button, Canvas, DoubleVar, Label, Radiobutton, Scale, Tk, IntVar
 
 font = "Century"
 tinyfontsize = 15
@@ -17,9 +17,15 @@ root.title("TREV-4 BSPD Test Board Control")
 root.configure(background=backgroundcolor)
 root.resizable(False, False)
 
+sliderPercentDisplay = True
+
 global arduino
 arduino = None
 comnumber = IntVar()
+voltage1 = DoubleVar()
+voltage2 = DoubleVar()
+percentage1 = DoubleVar()
+percentage2 = DoubleVar()
 comnumber.set(1)
 
 
@@ -29,7 +35,7 @@ def changeCom(update):
         if not arduino == None:
             arduino.close()
         port = "COM" + str(comnumber.get())
-        arduino = serial.Serial(port=port, baudrate=9600, timeout=1)
+        arduino = serial.Serial(port=port, baudrate=115200, timeout=1)
         connectFeedback.config(bg="green")
         return True
     except serial.serialutil.SerialException:
@@ -58,6 +64,43 @@ def flashCOMIndicator():
         root.after(200, flashCOMIndicator)
     else:
         connectFeedback.config(bg="red")
+
+def sliderUpdate(slider):
+    match slider:
+        case 1:
+            percentage1.set((voltage1.get() / 5) * 100)
+            pot1Voltage.config(text=str(format(voltage1.get(), f".{3}f")) + " V")
+            pot1Value.config(text=str(format(percentage1.get(), f".{1}f")) + " %")
+        case 2:
+            percentage2.set((voltage2.get() / 5) * 100)
+            pot2Voltage.config(text=str(format(voltage2.get(), f".{3}f")) + " V")
+            pot2Value.config(text=str(format((voltage2.get() / 5) * 100, f".{1}f")) + " %")
+        case 3:
+            voltage1.set((percentage1.get()/100)*5)
+            pot1Voltage.config(text=str(format(voltage1.get(), f".{3}f")) + " V")
+            pot1Value.config(text=str(format(percentage1.get(), f".{1}f") + " %"))
+        case 4:
+            voltage2.set((percentage2.get()/100)*5)
+            pot2Voltage.config(text=str(format(voltage2.get(), f".{3}f")) + " V")
+            pot2Value.config(text=str(format(percentage2.get(), f".{1}f") + " %"))
+
+def toggleSliderType():
+    global sliderPercentDisplay
+    if sliderPercentDisplay:
+        currentSliderState.config(text="Current State: Voltage")
+        potSliderPercent3.grid_forget()
+        potSliderPercent4.grid_forget()
+        potSlider1.grid(column=6, row=4)
+        potSlider2.grid(column=7, row=4)
+        sliderPercentDisplay = False
+    else:
+        currentSliderState.config(text="Current State: Percent")
+        potSlider1.grid_forget()
+        potSlider2.grid_forget()
+        potSliderPercent3.grid(column=6, row=4)
+        potSliderPercent4.grid(column=7, row=4)
+        sliderPercentDisplay = True
+
 
 
 R1 = Radiobutton(
@@ -163,8 +206,8 @@ connectFeedback = Canvas(root, width=150, height=150, bg="red")
 
 
 comLabel.grid(column=1, row=1, pady=25, columnspan=5)
-connectLabel.grid(column=2, row=3, rowspan=2)
-connectFeedback.grid(column=2, row=4, rowspan=10)
+connectLabel.grid(column=2, row=2, rowspan=2)
+connectFeedback.grid(column=2, row=3, rowspan=10)
 R1.grid(column=1, row=2, padx=10)
 R2.grid(column=1, row=3, padx=10)
 R3.grid(column=1, row=4, padx=10)
@@ -216,8 +259,69 @@ pot2Voltage = Label(
     background=backgroundcolor,
     fg=titletextcolor,
 )
-slider_horizontal = Scale(root, from_=0, to=100, orient="horizontal")
-slider_horizontal = Scale(root, from_=0, to=100, orient="horizontal")
+potSlider1 = Scale(
+    root,
+    variable=voltage1,
+    from_=0,
+    to=5,
+    orient=HORIZONTAL,
+    length=150,
+    resolution=.005,
+    command=lambda slider: sliderUpdate(1),
+    background=backgroundcolor,
+    fg=textcolor,
+)
+potSlider2 = Scale(
+    root,
+    variable=voltage2,
+    from_=0,
+    to=5,
+    orient=HORIZONTAL,
+    length=150,
+    resolution=.005,
+    command=lambda slider:  sliderUpdate(2),
+    background=backgroundcolor,
+    fg=textcolor,
+)
+potSliderPercent3 = Scale(
+    root,
+    variable=percentage1,
+    from_=0,
+    to=100,
+    orient=HORIZONTAL,
+    length=150,
+    resolution=.1,
+    command=lambda slider: sliderUpdate(3),
+    background=backgroundcolor,
+    fg=textcolor,
+)
+potSliderPercent4 = Scale(
+    root,
+    variable=percentage2,
+    from_=0,
+    to=100,
+    orient=HORIZONTAL,
+    length=150,
+    resolution=.1,
+    command=lambda slider:  sliderUpdate(4),
+    background=backgroundcolor,
+    fg=textcolor,
+)
+toggleSliderButton = Button(
+    root,
+    text="Toggle Input Type",
+    font=("font", smallfontsize),
+    background=backgroundcolor,
+    fg=textcolor,
+    command=toggleSliderType,
+)
+currentSliderState = Label(
+    root,
+    text="Current State: Percentage",
+    font=("font", smallfontsize),
+    background=backgroundcolor,
+    fg=textcolor,
+)
 
 pot1Lable.grid(column=6, row=1, padx=10)
 pot1Value.grid(column=6, row=2, padx=10)
@@ -225,6 +329,10 @@ pot1Voltage.grid(column=6, row=3, padx=10)
 pot2Lable.grid(column=7, row=1, padx=10)
 pot2Value.grid(column=7, row=2, padx=10)
 pot2Voltage.grid(column=7, row=3, padx=10)
+potSliderPercent3.grid(column=6, row=4)
+potSliderPercent4.grid(column=7, row=4)
+currentSliderState.grid(column=6, columnspan=3, row=5, rowspan=2)
+toggleSliderButton.grid(column=6, columnspan=3, row=6, rowspan=2, padx=10, pady=10)
 
 comnumber.set(1)
 root.mainloop()
